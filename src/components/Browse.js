71dc5f130d67b0d0285a1fc336339dae6fc89e41
Pranav Mudholkar <pranavmudholkar455@gmail.com>
2025-09-08 22:37:35 +0530

    integrated the video API from TMDB

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,15 @@
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryComponent from "./SecondaryComponent";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryComponent />
     </div>
   );
 };
